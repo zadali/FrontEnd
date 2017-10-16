f262ac8f5bc6f69b5c64cc292a2af5c0dfda5c2e
DaveEveritt <deveritt@innotts.co.uk>
2017-10-16 08:44:15 +0100

    updated js with comments

--- js/greeting.js
+++ js/greeting.js
@@ -1,16 +1,27 @@
 (function(){
-
-  var myName = document.getElementById("my-name"); 
-  var userName = document.getElementById("user-name"); 
-  var getName = document.getElementById("get-name");
-  var myHeader = document.getElementById("my-header");
+/* 
+  everything is contained inside a function expression that's
+  immediately invoked when the script loads in the page
+*/
   
+  /*
+    declare all the variables using 'let'. These get a reference
+    to the HTML elements we want by finding their ID
+  */
+  let myName = document.getElementById("my-name"); 
+  let userName = document.getElementById("user-name"); 
+  let getName = document.getElementById("get-name");
+  let myHeader = document.getElementById("my-header");
+  
+  // changes the text of the elements and adds a class
   function PerformGreeting() {
     myName.innerHTML = userName.value;
     myHeader.innerHTML = userName.value;
     myHeader.classList.add("change-me");
     return false;
   }
+
+  // 'listens' for the user to submit the form
   if (typeof event === "undefined") {
     getName.onsubmit = PerformGreeting; //Firefox
   } else {
