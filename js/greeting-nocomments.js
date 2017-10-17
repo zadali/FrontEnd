c3832d1adcd53bca21c76b4bb58f121a367372ae
Zad Bhatti <P15182321@LEC-ADMIN.dmu.ac.uk>
2017-10-17 15:42:31 +0100

    added new header

--- js/greeting-nocomments.js
+++ js/greeting-nocomments.js
@@ -2,9 +2,11 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let myHeader = document.getElementById("my-header");
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	myHeader.innerHTML = userName.value;
     event.preventDefault();
     return false;
   }
